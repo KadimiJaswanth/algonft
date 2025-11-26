71845e95513c384f7d50b5280193658d2f7cd538
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:52 +0530

    Add src/stories/HeaderVariantC.stories.tsx

---
+++ src/stories/HeaderVariantC.stories.tsx
@@ -0,0 +1,7 @@
+// Story: HeaderVariantC
+// TODO: demo header variants in Storybook
+import React from 'react';
+import HeaderVariantC from '../components/HeaderVariantC';
+
+export default { title: 'Header/VariantC' };
+export const Default = () => <HeaderVariantC />;
