80514550bf9cbc811e5d80ea9b0d1b524acb2fb0
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:31 +0530

    Add src/components/ToastStack.tsx

---
+++ src/components/ToastStack.tsx
@@ -0,0 +1,7 @@
+// ToastStack — stack of transient toasts
+// TODO: integrate with global toast provider (client-only)
+import React from "react";
+
+const ToastStack: React.FC = () => <div className="fixed bottom-4 right-4">Toasts</div>;
+
+export default ToastStack;
