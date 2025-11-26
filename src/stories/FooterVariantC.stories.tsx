364a165e1d4edb808bc51806487a8ca6867debf5
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-26 19:03:52 +0530

    Add src/stories/FooterVariantC.stories.tsx

---
+++ src/stories/FooterVariantC.stories.tsx
@@ -0,0 +1,6 @@
+// Story: FooterVariantC
+import React from 'react';
+import FooterVariantC from '../components/FooterVariantC';
+
+export default { title: 'Footer/VariantC' };
+export const Default = () => <FooterVariantC />;
