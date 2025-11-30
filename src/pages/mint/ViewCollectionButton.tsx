9d3e45ccda397e259f00ba4c1388c268d44b56bd
Kadimi Jaswanth <rishikadimi7@gmail.com>
2025-11-30 16:20:27 +0530

    Add src/pages/mint/ViewCollectionButton.tsx

---
+++ src/pages/mint/ViewCollectionButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ViewCollectionButton: React.FC = () => {
+  return (
+    <button className="px-4 py-2 bg-transparent border border-[#4dd0e1] rounded-lg text-[#4dd0e1]">View Collection</button>
+  );
+};
+
+export default ViewCollectionButton;
